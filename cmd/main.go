cdb7c54c076ed7671ab945d2a5fc950204852f13
Awais Saeed <awaissaeed530@gmail.com>
2024-03-04 14:00:11 +0500

    add aws sdk

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/route53domains"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := route53domains.NewFromConfig(cfg, func(o *route53domains.Options) {
+		o.Region = "us-east-1"
+	})
+
+	domain := "awaissaeed.com"
+	output, err := client.CheckDomainAvailability(context.TODO(), &route53domains.CheckDomainAvailabilityInput{
+		DomainName: &domain,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%s", output.Availability)
 }
